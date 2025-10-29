79db325bfc3eecb2dd273be9c438849590f03c83
Sami1233hf <achaksami383@gmail.com>
2025-10-29 09:13:17 +0100

    Initial commit
    Add Sami Portfolio project with HTML, CSS, and JavaScript files

---
+++ scripte.js
@@ -0,0 +1,5 @@
+// Simple form submit action
+document.getElementById("contact-form").addEventListener("submit", function(e) {
+  e.preventDefault();
+  alert("Thank you for your message! I'll get back to you soon.");
+});
